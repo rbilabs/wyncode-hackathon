fb5a6e13a3c08bcec469bcf04d2baa91406c6419
Robert Beavin <rmbeavin@gmail.com>
2019-02-13 12:14:17 -0500

    initial commit

--- src/Wheel.js
+++ src/Wheel.js
@@ -10,16 +10,16 @@ let padding = {top:20, right:40, bottom:0, left:0},
             oldpick = [];
             // color = d3.scale.category20();
         let data = [
-                    {"label":"Popeyes",  "value":1,  "question":"What CSS property is used for specifying the area between the content and its border?"}, // padding
-                    {"label":"IMAC PRO",  "value":2,  "question":"What CSS property is used for changing the font?"}, //font-family
-                    {"label":"SUZUKI",  "value":3,  "question":"What CSS property is used for changing the color of text?"}, //color
-                    {"label":"HONDA",  "value":4,  "question":"What CSS property is used for changing the boldness of text?"}, //font-weight
-                    {"label":"FERRARI",  "value":5,  "question":"What CSS property is used for changing the size of text?"}, //font-size
-                    {"label":"APARTMENT",  "value":6,  "question":"What CSS property is used for changing the background color of a box?"}, //background-color
-                    {"label":"IPAD PRO",  "value":7,  "question":"Which word is used for specifying an HTML tag that is inside another tag?"}, //nesting
-                    {"label":"LAND",  "value":8,  "question":"Which side of the box is the third number in: margin:1px 1px 1px 1px; ?"}, //bottom
-                    {"label":"MOTOROLLA",  "value":9,  "question":"What are the fonts that don't have serifs at the ends of letters called?"}, //sans-serif
-                    {"label":"BMW", "value":10, "question":"With CSS selectors, what character prefix should one use to specify a class?"}
+                    {"label":"Popeyes",  "value":1,  "question":"--"}, 
+                    {"label":"--",  "value":2,  "question":"---"}, 
+                    {"label":"--",  "value":3,  "question":"---"}, 
+                    {"label":"--",  "value":4,  "question":"---"}, 
+                    {"label":"--",  "value":5,  "question":"---"}, 
+                    {"label":"--",  "value":6,  "question":"---"}, 
+                    {"label":"--",  "value":7,  "question":"---"}, 
+                    {"label":"--",  "value":8,  "question":"---"}, 
+                    {"label":"--",  "value":9,  "question":"---"}, 
+                    {"label":"--", "value":10, "question":"---"}
         ];
         let svg = d3.select('#chart')
             .append("svg")
